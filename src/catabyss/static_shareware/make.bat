9d8a70b7922aed72fc5a744ba338b603bd964060
NY00123 <ny00@outlook.com>
2014-11-15 11:49:32 +0200

    Misc. cleanups for static data

--- src/catabyss/static_shareware/make.bat
+++
@@ -1,7 +0,0 @@
-bcc makeobj.c
-makeobj c audiodct.abs ..\absadict.obj _audiodict
-makeobj f audiohhd.abs ..\absahead.obj _AudioHeader _audiohead
-makeobj c egadict.abs ..\absedict.obj _EGAdict
-makeobj f egahead.abs ..\absehead.obj EGA_grafixheader _EGAhead
-makeobj f mtemp.tmp ..\absmhead.obj MapHeader _maphead
-
