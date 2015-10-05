ReflectionHLE/ReflectionHLE

commit b5bb0580aa35b034fc895dba86de117fb4823a02

Display refresh bugfix in BEL_ST_UpdateHostDisplay; Always use events for mouse and joysticks.
Events are usual for alternative controller scheme while emulating keyboard key presses
or releases, and same with some mouse actions, so just use these. Among other changes,
BEL_ST_AltControlScheme_HandleEntry should be ready to handle (emulated) mouse motion as well.

src/be_st_sdl_graphics.c
@@ -679,6 +679,8 @@ int BEL_ST_MoveUpInTextInputUI(void)
 		g_sdlKeyboardUISelectedKeyY = ALTCONTROLLER_TEXTINPUT_KEYS_HEIGHT-1;
 	}
 	BEL_ST_ToggleTextInputUIKey(g_sdlKeyboardUISelectedKeyX, g_sdlKeyboardUISelectedKeyY, true, false);
+
+	g_sdlForceGfxControlUiRefresh = true;
 	return origScanCode;
 }
 
@@ -694,6 +696,8 @@ int BEL_ST_MoveDownInTextInputUI(void)
 		g_sdlKeyboardUISelectedKeyY = 0;
 	}
 	BEL_ST_ToggleTextInputUIKey(g_sdlKeyboardUISelectedKeyX, g_sdlKeyboardUISelectedKeyY, true, false);
+
+	g_sdlForceGfxControlUiRefresh = true;
 	return origScanCode;
 }
 
@@ -709,6 +713,8 @@ int BEL_ST_MoveLeftInTextInputUI(void)
 		g_sdlKeyboardUISelectedKeyX = ALTCONTROLLER_TEXTINPUT_KEYS_WIDTH-1;
 	}
 	BEL_ST_ToggleTextInputUIKey(g_sdlKeyboardUISelectedKeyX, g_sdlKeyboardUISelectedKeyY, true, false);
+
+	g_sdlForceGfxControlUiRefresh = true;
 	return origScanCode;
 }
 
@@ -724,6 +730,8 @@ int BEL_ST_MoveRightInTextInputUI(void)
 		g_sdlKeyboardUISelectedKeyX = 0;
 	}
 	BEL_ST_ToggleTextInputUIKey(g_sdlKeyboardUISelectedKeyX, g_sdlKeyboardUISelectedKeyY, true, false);
+
+	g_sdlForceGfxControlUiRefresh = true;
 	return origScanCode;
 }
 
@@ -737,6 +745,8 @@ void BEL_ST_MoveUpInDebugKeysUI(void)
 		g_sdlKeyboardUISelectedKeyY = ALTCONTROLLER_DEBUGKEYS_KEYS_HEIGHT-1;
 	}
 	BEL_ST_ToggleDebugKeysKey(g_sdlKeyboardUISelectedKeyX, g_sdlKeyboardUISelectedKeyY, true, g_sdlDebugKeysPressed[g_sdlKeyboardUISelectedKeyY][g_sdlKeyboardUISelectedKeyX]);
+
+	g_sdlForceGfxControlUiRefresh = true;
 }
 
 void BEL_ST_MoveDownInDebugKeysUI(void)
@@ -749,6 +759,8 @@ void BEL_ST_MoveDownInDebugKeysUI(void)
 		g_sdlKeyboardUISelectedKeyY = 0;
 	}
 	BEL_ST_ToggleDebugKeysKey(g_sdlKeyboardUISelectedKeyX, g_sdlKeyboardUISelectedKeyY, true, g_sdlDebugKeysPressed[g_sdlKeyboardUISelectedKeyY][g_sdlKeyboardUISelectedKeyX]);
+
+	g_sdlForceGfxControlUiRefresh = true;
 }
 
 void BEL_ST_MoveLeftInDebugKeysUI(void)
@@ -761,6 +773,8 @@ void BEL_ST_MoveLeftInDebugKeysUI(void)
 		g_sdlKeyboardUISelectedKeyX = ALTCONTROLLER_DEBUGKEYS_KEYS_WIDTH-1;
 	}
 	BEL_ST_ToggleDebugKeysKey(g_sdlKeyboardUISelectedKeyX, g_sdlKeyboardUISelectedKeyY, true, g_sdlDebugKeysPressed[g_sdlKeyboardUISelectedKeyY][g_sdlKeyboardUISelectedKeyX]);
+
+	g_sdlForceGfxControlUiRefresh = true;
 }
 
 void BEL_ST_MoveRightInDebugKeysUI(void)
@@ -773,6 +787,8 @@ void BEL_ST_MoveRightInDebugKeysUI(void)
 		g_sdlKeyboardUISelectedKeyX = 0;
 	}
 	BEL_ST_ToggleDebugKeysKey(g_sdlKeyboardUISelectedKeyX, g_sdlKeyboardUISelectedKeyY, true, g_sdlDebugKeysPressed[g_sdlKeyboardUISelectedKeyY][g_sdlKeyboardUISelectedKeyX]);
+
+	g_sdlForceGfxControlUiRefresh = true;
 }
 
 
@@ -796,7 +812,8 @@ int BEL_ST_ToggleKeyPressInTextInputUI(bool *pToggle)
 	g_sdlTextInputIsKeyPressed = *pToggle;
 
 	BEL_ST_ToggleTextInputUIKey(g_sdlKeyboardUISelectedKeyX, g_sdlKeyboardUISelectedKeyY, true, *pToggle);
-	//g_sdlForceGfxControlUiRefresh = true;
+
+	g_sdlForceGfxControlUiRefresh = true;
 
 	// Shift key is a special case
 	if ((g_sdlDOSScanCodeTextInputLayout[g_sdlKeyboardUISelectedKeyY][g_sdlKeyboardUISelectedKeyX] == BE_ST_SC_LSHIFT))
@@ -811,6 +828,8 @@ int BEL_ST_ToggleKeyPressInDebugKeysUI(bool *pToggle)
 	*pToggle = g_sdlDebugKeysPressed[g_sdlKeyboardUISelectedKeyY][g_sdlKeyboardUISelectedKeyX];
 
 	BEL_ST_ToggleDebugKeysKey(g_sdlKeyboardUISelectedKeyX, g_sdlKeyboardUISelectedKeyY, true, *pToggle);
+
+	g_sdlForceGfxControlUiRefresh = true;
 
 	return (int)g_sdlDOSScanCodeDebugKeysLayout[g_sdlKeyboardUISelectedKeyY][g_sdlKeyboardUISelectedKeyX];
 }
@@ -1550,8 +1569,7 @@ void BEL_ST_UpdateHostDisplay(void)
 		if (!g_sdlDoRefreshGfxOutput && (wereBlinkingCharsShown == areBlinkingCharsShown) && (wasBlinkingCursorShown == isBlinkingCursorShown))
 		{
 			if (g_sdlForceGfxControlUiRefresh)
-				BEL_ST_FinishHostDisplayUpdate();
-			return;
+				goto dorefresh;
 		}
 		/****** Do update ******/
 		wereBlinkingCharsShown = areBlinkingCharsShown;
@@ -1625,8 +1643,7 @@ void BEL_ST_UpdateHostDisplay(void)
 		if (!g_sdlDoRefreshGfxOutput)
 		{
 			if (g_sdlForceGfxControlUiRefresh)
-				BEL_ST_FinishHostDisplayUpdate();
-			return;
+				goto dorefresh;
 		}
 		// That's easy now since there isn't a lot that can be done...
 		void *pixels;
@@ -1644,8 +1661,7 @@ void BEL_ST_UpdateHostDisplay(void)
 		if (!g_sdlDoRefreshGfxOutput)
 		{
 			if (g_sdlForceGfxControlUiRefresh)
-				BEL_ST_FinishHostDisplayUpdate();
-			return;
+				goto dorefresh;
 		}
 		uint16_t currLineFirstByte = (g_sdlScreenStartAddress + g_sdlPelPanning/8) % 0x10000;
 		uint8_t panningWithinInByte = g_sdlPelPanning%8;
@@ -1712,8 +1728,7 @@ void BEL_ST_UpdateHostDisplay(void)
 			{
 				g_sdlDoRefreshGfxOutput = false;
 				if (g_sdlForceGfxControlUiRefresh)
-					BEL_ST_FinishHostDisplayUpdate();
-				return;
+					goto dorefresh;
 			}
 		}
 		void *pixels;
@@ -1729,6 +1744,8 @@ void BEL_ST_UpdateHostDisplay(void)
 
 	g_sdlDoRefreshGfxOutput = false;
 	SDL_UnlockTexture(g_sdlTexture);
+
+dorefresh:
 	SDL_SetRenderDrawColor(g_sdlRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
 	SDL_RenderClear(g_sdlRenderer);
 	SDL_SetRenderDrawColor(g_sdlRenderer, (g_sdlEGACurrBGRAPaletteAndBorder[16]>>16)&0xFF, (g_sdlEGACurrBGRAPaletteAndBorder[16]>>8)&0xFF, g_sdlEGACurrBGRAPaletteAndBorder[16]&0xFF, SDL_ALPHA_OPAQUE);

src/be_st_sdl_audio_timer.c
@@ -343,7 +343,7 @@ static inline void PCSpeakerUpdateOne(int16_t *stream, int length)
 		{
 			g_sdlBeepHalfCycleCounter %= g_sdlBeepHalfCycleCounterUpperBound;
 			// 32767 - too loud
-			g_sdlCurrentBeepSample = 24575-g_sdlCurrentBeepSample;
+			g_sdlCurrentBeepSample ^= 24575;
 		}
 	}
 }

src/be_st_sdl_launcher.c
@@ -31,10 +31,8 @@ extern SDL_Rect g_sdlAspectCorrectionBorderedRect;
 extern const char *g_sdlWindowTitle;
 extern const int g_sdlJoystickAxisBinaryThreshold, g_sdlJoystickAxisDeadZone, g_sdlJoystickAxisMax, g_sdlJoystickAxisMaxMinusDeadZone;
 
-extern bool g_sdlControllersButtonsStates[BE_ST_MAXJOYSTICKS][SDL_CONTROLLER_BUTTON_MAX];
-// We may optionally use analog axes as buttons (e.g., using stick as arrow keys, triggers as buttons)
-extern bool g_sdlControllersAxesStates[BE_ST_MAXJOYSTICKS][SDL_CONTROLLER_AXIS_MAX][2];
-
+// Need these for triggers
+static bool g_sdlLauncherTriggerBinaryStates[2];
 
 static uint8_t g_sdlLauncherGfxCache[BE_LAUNCHER_PIX_WIDTH*BE_LAUNCHER_PIX_HEIGHT]; // Launcher gets pointer to this for drawing
 static bool g_sdlLauncherGfxCacheMarked = false;
@@ -372,8 +370,7 @@ void BE_ST_Launcher_Prepare(void)
 		if (SDL_IsGameController(i))
 			g_sdlControllers[i] = SDL_GameControllerOpen(i);
 
-	memset(g_sdlControllersButtonsStates, 0, sizeof(g_sdlControllersButtonsStates));
-	memset(g_sdlControllersAxesStates, 0, sizeof(g_sdlControllersAxesStates));
+	memset(g_sdlLauncherTriggerBinaryStates, 0, sizeof(g_sdlLauncherTriggerBinaryStates));
 
 	// Set fullscreen value
 	g_beVideoSettingsMenuItem_Fullscreen.choice = g_refKeenCfg.isFullscreen;
@@ -489,6 +486,15 @@ void BE_ST_Launcher_Shutdown(void)
 			g_sdlControllers[i] = NULL;
 		}
 
+	/* Poll events just in case (e.g., clean up old controller events) */
+	SDL_Event event;
+	bool doExit = false;
+	while (SDL_PollEvent(&event))
+	{
+		if (event.type == SDL_QUIT)
+			doExit = true;
+	}
+
 	SDL_DestroyTexture(g_sdlTexture);
 	g_sdlTexture = NULL;
 	SDL_DestroyTexture(g_sdlTargetTexture);
@@ -556,6 +562,9 @@ void BE_ST_Launcher_Shutdown(void)
 	g_refKeenCfg.altControlScheme.actionMappings[BE_ST_CTRL_CFG_BUTMAP_FUNCKEYS] = g_beControllerSettingsMenuItem_Action_FuncKeys.choice;
 #endif
 	g_refKeenCfg.altControlScheme.actionMappings[BE_ST_CTRL_CFG_BUTMAP_DEBUGKEYS] = g_beControllerSettingsMenuItem_Action_DebugKeys.choice;
+
+	if (doExit)
+		BE_ST_QuickExit();
 }
 
 
@@ -719,6 +728,10 @@ void BE_ST_Launcher_RunEventLoop(void)
 					}
 				break;
 
+			case SDL_CONTROLLERAXISMOTION: // Need this so a pressed trigger is ignored once user gets to choose a button for in-game action
+				if ((event.caxis.axis == SDL_CONTROLLER_AXIS_TRIGGERLEFT) || (event.caxis.axis == SDL_CONTROLLER_AXIS_TRIGGERRIGHT))
+					g_sdlLauncherTriggerBinaryStates[event.caxis.axis - SDL_CONTROLLER_AXIS_TRIGGERLEFT] = (event.caxis.value >= g_sdlJoystickAxisBinaryThreshold);
+				break;
 			case SDL_CONTROLLERBUTTONDOWN:
 				switch (event.cbutton.button)
 				{
@@ -760,49 +773,6 @@ void BE_ST_Launcher_RunEventLoop(void)
 				BE_ST_Launcher_Shutdown();
 				BE_ST_QuickExit();
 				break;
-			}
-		}
-
-		// If SDL_GameController is used, we don't poll for its events. We also limit ourselves to buttons only (not even triggers).
-		SDL_GameControllerUpdate();
-
-		for (int i = 0; i < BE_ST_MAXJOYSTICKS; ++i)
-		{
-			if (g_sdlControllers[i])
-			{
-				for (int but = 0; but < SDL_CONTROLLER_BUTTON_MAX; ++but)
-				{
-					bool isPressed = SDL_GameControllerGetButton(g_sdlControllers[i], (SDL_GameControllerButton)but);
-					if (isPressed != g_sdlControllersButtonsStates[i][but])
-					{
-						g_sdlControllersButtonsStates[i][but] = isPressed;
-						if (isPressed)
-						{
-							switch (but)
-							{
-							case SDL_CONTROLLER_BUTTON_DPAD_RIGHT:
-								BE_Launcher_HandleInput_ButtonRight();
-								break;
-							case SDL_CONTROLLER_BUTTON_DPAD_LEFT:
-								BE_Launcher_HandleInput_ButtonLeft();
-								break;
-							case SDL_CONTROLLER_BUTTON_DPAD_DOWN:
-								BE_Launcher_HandleInput_ButtonDown();
-								break;
-							case SDL_CONTROLLER_BUTTON_DPAD_UP:
-								BE_Launcher_HandleInput_ButtonUp();
-								break;
-							case SDL_CONTROLLER_BUTTON_A:
-								BE_Launcher_HandleInput_ButtonActivate();
-								break;
-							case SDL_CONTROLLER_BUTTON_B:
-							case SDL_CONTROLLER_BUTTON_BACK:
-								BE_Launcher_HandleInput_ButtonBack();
-								break;
-							}
-						}
-					}
-				}
 			}
 		}
 
@@ -906,6 +876,38 @@ void BE_ST_Launcher_WaitForControllerButton(BEMenuItem *menuItem)
 					}
 				break;
 
+			case SDL_CONTROLLERAXISMOTION:
+				if ((event.caxis.axis == SDL_CONTROLLER_AXIS_TRIGGERLEFT) || (event.caxis.axis == SDL_CONTROLLER_AXIS_TRIGGERRIGHT))
+				{
+					int triggerNum = event.caxis.axis - SDL_CONTROLLER_AXIS_TRIGGERLEFT;
+					bool prevBinaryState = g_sdlLauncherTriggerBinaryStates[triggerNum];
+					g_sdlLauncherTriggerBinaryStates[triggerNum] = (event.caxis.value >= g_sdlJoystickAxisBinaryThreshold);
+					if (!prevBinaryState && g_sdlLauncherTriggerBinaryStates[triggerNum])
+					{
+						choice = SDL_CONTROLLER_BUTTON_MAX + triggerNum; // HACK
+						keepRunning = false;
+					}
+				}
+				break;
+			case SDL_CONTROLLERBUTTONDOWN:
+				switch (event.cbutton.button)
+				{
+					case SDL_CONTROLLER_BUTTON_A:
+					case SDL_CONTROLLER_BUTTON_B:
+					case SDL_CONTROLLER_BUTTON_X:
+					case SDL_CONTROLLER_BUTTON_Y:
+					case SDL_CONTROLLER_BUTTON_LEFTSTICK:
+					case SDL_CONTROLLER_BUTTON_RIGHTSTICK:
+					case SDL_CONTROLLER_BUTTON_LEFTSHOULDER:
+					case SDL_CONTROLLER_BUTTON_RIGHTSHOULDER:
+						choice = event.cbutton.button;
+						// Fall-through
+					default:
+						keepRunning = false;
+						break;
+				}
+				break;
+
 			case SDL_WINDOWEVENT:
 				switch (event.window.event)
 				case  SDL_WINDOWEVENT_RESIZED:
@@ -922,61 +924,6 @@ void BE_ST_Launcher_WaitForControllerButton(BEMenuItem *menuItem)
 				BE_ST_Launcher_Shutdown();
 				BE_ST_QuickExit();
 				break;
-			}
-		}
-
-		// We don't poll for SDL_GameController events.
-		SDL_GameControllerUpdate();
-
-		for (int i = 0; i < BE_ST_MAXJOYSTICKS; ++i)
-		{
-			if (g_sdlControllers[i])
-			{
-				// Check some of the buttons
-				for (int but = 0; but < SDL_CONTROLLER_BUTTON_MAX; ++but)
-				{
-					bool isPressed = SDL_GameControllerGetButton(g_sdlControllers[i], (SDL_GameControllerButton)but);
-					if (isPressed != g_sdlControllersButtonsStates[i][but])
-					{
-						g_sdlControllersButtonsStates[i][but] = isPressed;
-						if (isPressed)
-						{
-							switch (but)
-							{
-							case SDL_CONTROLLER_BUTTON_A:
-							case SDL_CONTROLLER_BUTTON_B:
-							case SDL_CONTROLLER_BUTTON_X:
-							case SDL_CONTROLLER_BUTTON_Y:
-							case SDL_CONTROLLER_BUTTON_LEFTSTICK:
-							case SDL_CONTROLLER_BUTTON_RIGHTSTICK:
-							case SDL_CONTROLLER_BUTTON_LEFTSHOULDER:
-							case SDL_CONTROLLER_BUTTON_RIGHTSHOULDER:
-								choice = but;
-								// Fall-through
-							default:
-								keepRunning = false;
-								break;
-							}
-						}
-					}
-				}
-				// Check trigger axes, positive sides, only
-				for (int axis = SDL_CONTROLLER_AXIS_TRIGGERLEFT; axis <= SDL_CONTROLLER_AXIS_TRIGGERRIGHT; ++axis)
-				{
-					int axisVal = (int)SDL_GameControllerGetAxis(g_sdlControllers[i], (SDL_GameControllerAxis)axis);
-					// Is pressed in the positive direction?
-					bool isPosPressed = (axisVal >= g_sdlJoystickAxisBinaryThreshold);
-					if (isPosPressed != g_sdlControllersAxesStates[i][axis][1])
-					{
-						g_sdlControllersAxesStates[i][axis][1] = isPosPressed;
-						if (isPosPressed)
-						{
-							// HACK
-							choice = SDL_CONTROLLER_BUTTON_MAX + axis - SDL_CONTROLLER_AXIS_TRIGGERLEFT;
-							keepRunning = false;
-						}
-					}
-				}
 			}
 		}
 
@@ -1000,5 +947,4 @@ void BE_ST_Launcher_WaitForControllerButton(BEMenuItem *menuItem)
 	menuItem->choice = choice;
 	// Reset
 	g_sdlLauncherGfxCacheMarked = true;
-	memset(g_sdlControllersAxesStates, 0, sizeof(g_sdlControllersAxesStates));
 }

src/id91_11/cat_all/altcontroller.c
@@ -427,14 +427,14 @@ void RefKeen_PrepareAltControllerScheme(void)
 		if (g_ingame_altcontrol_button0mappings[0])
 		{
 			g_ingame_altcontrol_button0mappings[0]->mapClass = BE_ST_CTRL_MAP_MOUSEBUTTON;
-			g_ingame_altcontrol_button0mappings[0]->val = 0;
+			g_ingame_altcontrol_button0mappings[0]->val = 1;
 			// Game code shouldn't touch this
 			g_ingame_altcontrol_button0mappings[0] = NULL;
 		}
 		if (g_ingame_altcontrol_button1mappings[0])
 		{
 			g_ingame_altcontrol_button1mappings[0]->mapClass = BE_ST_CTRL_MAP_MOUSEBUTTON;
-			g_ingame_altcontrol_button1mappings[0]->val = 1;
+			g_ingame_altcontrol_button1mappings[0]->val = 2;
 			// Game code shouldn't touch this
 			g_ingame_altcontrol_button1mappings[0] = NULL;
 		}

commit aa858912d48b1c333c23f9918775ba0802bcf863

Fix unnecessary refreshes bug and improve in BE_ST_SetScreenStartAddress
Bug introduced in revision b5bb0580aa35b034fc895dba86de117fb4823a02,
while the BE_ST_SetScreenStartAddress change further reduces host
display updates with the same data. Detected while using a joystick
with the "classic" controller scheme inside Cat3D's US_ControlPanel
(selection changed much slower than expected), with VSync turned on.

src/be_st_sdl_graphics.c
@@ -838,6 +838,8 @@ int BEL_ST_ToggleShiftStateInTextInputUI(bool *pToggle)
 
 	BEL_ST_RedrawWholeTextInputUI();
 
+	g_sdlForceGfxControlUiRefresh = true;
+
 	return BE_ST_SC_LSHIFT;
 }
 
@@ -1244,7 +1246,7 @@ void BE_ST_SetGfxOutputRects(void)
 void BE_ST_SetScreenStartAddress(uint16_t crtc)
 {
 	g_sdlScreenStartAddress = crtc;
-	g_sdlDoRefreshGfxOutput = true;
+	g_sdlDoRefreshGfxOutput |= (g_sdlScreenStartAddress != crtc);
 }
 
 uint8_t *BE_ST_GetTextModeMemoryPtr(void)
@@ -1837,6 +1839,7 @@ void BEL_ST_UpdateHostDisplay(void)
 		{
 			if (g_sdlForceGfxControlUiRefresh)
 				goto dorefresh;
+			return;
 		}
 		/****** Do update ******/
 		wereBlinkingCharsShown = areBlinkingCharsShown;
@@ -1911,6 +1914,7 @@ void BEL_ST_UpdateHostDisplay(void)
 		{
 			if (g_sdlForceGfxControlUiRefresh)
 				goto dorefresh;
+			return;
 		}
 		// That's easy now since there isn't a lot that can be done...
 		void *pixels;
@@ -1929,6 +1933,7 @@ void BEL_ST_UpdateHostDisplay(void)
 		{
 			if (g_sdlForceGfxControlUiRefresh)
 				goto dorefresh;
+			return;
 		}
 		uint16_t currLineFirstByte = (g_sdlScreenStartAddress + g_sdlPelPanning/8) % 0x10000;
 		uint8_t panningWithinInByte = g_sdlPelPanning%8;
@@ -1996,6 +2001,7 @@ void BEL_ST_UpdateHostDisplay(void)
 				g_sdlDoRefreshGfxOutput = false;
 				if (g_sdlForceGfxControlUiRefresh)
 					goto dorefresh;
+				return;
 			}
 		}
 		void *pixels;