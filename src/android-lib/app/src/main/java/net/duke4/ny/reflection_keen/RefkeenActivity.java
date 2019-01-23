d4b2d79f0b079aa69d3e8a8f22a5753a4fc80ec9
NY00123 <ny00@outlook.com>
2019-01-24 00:30:05 +0200

    Let's remove this code from RefkeenActivity.java, since it's already in SDLActivity.java for most. Also make misc. changes to the manifests.

--- src/android-lib/app/src/main/java/net/duke4/ny/reflection_keen/RefkeenActivity.java
+++ src/android-lib/app/src/main/java/net/duke4/ny/reflection_keen/RefkeenActivity.java
@@ -88,31 +88,4 @@ public class RefkeenActivity extends SDLActivity implements ActivityCompat.OnReq
 			}
 		}
 	}
-
-	private void setStickyImmersionMode() {
-		if (android.os.Build.VERSION.SDK_INT >= 19) {
-			getWindow().getDecorView().setSystemUiVisibility(
-			    View.SYSTEM_UI_FLAG_LAYOUT_STABLE // 16
-			    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION // 16
-			    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN // 16
-			    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // 14
-			    | View.SYSTEM_UI_FLAG_FULLSCREEN // 16
-			    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY // 19
-			);
-		}
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		setStickyImmersionMode();
-	}
-
-	@Override
-	public void onWindowFocusChanged(boolean hasFocus) {
-		super.onWindowFocusChanged(hasFocus);
-		if (hasFocus) {
-			setStickyImmersionMode();
-		}
-	}
 }
