59b4153e5c5d92e8727920f45b6ea29c993efeed
NY00123 <ny00@outlook.com>
2020-06-12 01:20:05 +0300

    Add the refkeen_game_exe_main_funcs and refkeen_slidecat_exe_main_funcs arrays to be_gamedef_catadventures.h. They'll be used for the Catacomb Adventure Series. This also makes the first use of REFKEEN_HAS_VER* macros.

--- src/be_gamedef_catadventures.h
+++ src/be_gamedef_catadventures.h
@@ -9,3 +9,44 @@
 #ifdef REFKEEN_VER_CATAPOC
 #include "be_gamedef_catapoc.h"
 #endif
+
+// These MUST have the same order as in the BE_GameVer_T enum
+void (*refkeen_game_exe_main_funcs[BE_GAMEVER_LAST])(void) = {
+#ifdef REFKEEN_HAS_VER_KDREAMS
+#error "Undefined for Keen Dreams"
+#endif
+#ifdef REFKEEN_HAS_VER_CAT3D
+	&cat3d_exe_main,
+	&cat3d_exe_main,
+#endif
+#ifdef REFKEEN_HAS_VER_CATABYSS
+	&abysgame_exe_main,
+	&abysgame_exe_main,
+#endif
+#ifdef REFKEEN_HAS_VER_CATARM
+	&armgame_exe_main,
+#endif
+#ifdef REFKEEN_HAS_VER_CATAPOC
+	&apocgame_exe_main,
+#endif
+};
+
+void (*refkeen_slidecat_exe_main_funcs[BE_GAMEVER_LAST])(void) = {
+#ifdef REFKEEN_HAS_VER_KDREAMS
+#error "Undefined for Keen Dreams"
+#endif
+#ifdef REFKEEN_HAS_VER_CAT3D
+	NULL,
+	NULL,
+#endif
+#ifdef REFKEEN_HAS_VER_CATABYSS
+	&slidecat_exe_main,
+	&slidecat_exe_main,
+#endif
+#ifdef REFKEEN_HAS_VER_CATARM
+	&slidecat_exe_main,
+#endif
+#ifdef REFKEEN_HAS_VER_CATAPOC
+	&slidecat_exe_main,
+#endif
+};
