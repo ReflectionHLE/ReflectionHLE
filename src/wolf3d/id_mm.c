0e85a46ae8f76c32f8550dd2fd5a3bf983265e35
NY00123 <ny00@outlook.com>
2020-07-12 00:23:39 +0300

    Wolf3D: Add copyright headers to C and H files. Remaining ASM files will be handled later. The original sources from 1995 didn't have these headers, either way.

--- src/wolf3d/id_mm.c
+++ src/wolf3d/id_mm.c
@@ -1,3 +1,27 @@
+/* Wolfenstein 3D Source Code
+ * Copyright (C) 1993-1996 id Software, Inc.
+ * Copyright (C) 2020 NY00123
+ *
+ * This file is part of Wolfenstein 3D.
+ *
+ * Wolfenstein 3D is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Wolfenstein 3D is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * As an addendum, you can further use the Wolfenstein 3D Source Code under
+ * the terms of the limited use software license agreement for Wolfenstein 3D.
+ * See id-wolf3d.txt for these terms.
+ */
+
 // NEWMM.C
 
 /*
