a656ee61f27ae7fa70362bf56caf19927f1c1a49
NY00123 <ny00@outlook.com>
2020-09-13 21:18:47 +0300

    wolf3d/wl_play.c:RefKeen_Patch_wl_play: Add offset for Wolf3D shareware v1.4

--- src/wolf3d/wl_play.c
+++ src/wolf3d/wl_play.c
@@ -2004,6 +2004,8 @@ void RefKeen_Patch_wl_play(void)
 	refkeen_compat_wl_play_objoffset = 0xA73A;
 #elif (defined REFKEEN_VER_WL1AP12)
 	refkeen_compat_wl_play_objoffset = 0xA74A;
+#elif (defined REFKEEN_VER_WL1AP14)
+	refkeen_compat_wl_play_objoffset = 0xAB6E;
 #elif (defined REFKEEN_VER_SDMFG10)
 	refkeen_compat_wl_play_objoffset = 0xA224;
 #endif
