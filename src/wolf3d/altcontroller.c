8973cf6bb3f8ca2480e7910e272fb0c87bc88167
NY00123 <ny00@outlook.com>
2020-09-20 19:46:12 +0300

    Wolf3D: Add an initial altcontroller.c file with a mostly empty g_ingame_altcontrol_mapping_gameplay struct for now. Use it to toggle on relative mouse motion for now.

---
+++ src/wolf3d/altcontroller.c
@@ -0,0 +1,51 @@
+/* Wolfenstein 3D Source Code
+ * Copyright (C) 1993-1996 id Software, Inc.
+ * Copyright (C) 2020 NY00123
+ *
+ * This file is part of Wolfenstein 3D.
+ *
+ * Wolfenstein 3D is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Wolfenstein 3D is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * As an addendum, you can further use the Wolfenstein 3D Source Code under
+ * the terms of the limited use software license agreement for Wolfenstein 3D.
+ * See id-wolf3d.txt for these terms.
+ */
+
+// New file used for preparing and defining alternative controller schemes,
+// in case their definitions are configurable (possibly in more than one way)
+
+#include <string.h>
+#include "id_heads.h"
+
+REFKEEN_NS_B
+
+// TODO implement
+
+BE_ST_ControllerMapping g_ingame_altcontrol_mapping_gameplay = {
+	{},
+	NULL,
+	NULL,
+	{},
+	{},
+	false,
+	false,
+	true,
+};
+
+void PrepareGamePlayControllerMapping(void)
+{
+	BE_ST_AltControlScheme_PrepareControllerMapping(&g_ingame_altcontrol_mapping_gameplay);
+}
+
+REFKEEN_NS_E
