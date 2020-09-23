b4606be45302f03994f0e23e70ccf0476b9671c3
NY00123 <ny00@outlook.com>
2020-09-23 21:22:31 +0300

    Relicense code which is now identified as the Reflection Keen back-end under the terms of version 2.1 of the GNU Lesser General Public License, as well as any later version.

--- src/be_st.h
+++ src/be_st.h
@@ -1,20 +1,20 @@
 /* Copyright (C) 2014-2020 NY00123
  *
- * This file is part of Reflection Keen.
+ * This file is part of the Reflection Keen back-end.
  *
- * Reflection Keen is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * The Reflection Keen back-end is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 2.1
+ * of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * The Reflection Keen back-end is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Reflection Keen back-end. If not,
+ * see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef BE_ST_H
