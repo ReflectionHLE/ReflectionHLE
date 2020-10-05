afdc5934d4e314304bc5ebe0275fa1731d33107f
NY00123 <ny00@outlook.com>
2020-10-05 21:44:51 +0300

    Use be_filesystem_char.h in be_filesystem_dir.h

--- src/backend/filesystem/be_filesystem_dir.h
+++ src/backend/filesystem/be_filesystem_dir.h
@@ -1,29 +1,16 @@
 #ifndef BE_FILESYSTEM_DIR_H
 #define BE_FILESYSTEM_DIR_H
 
-#include "refkeen.h"
+#include "be_filesystem_char.h"
 
 typedef void *BE_DIR_T;
 
-#ifdef REFKEEN_PLATFORM_WINDOWS
-
-BE_DIR_T BEL_Cross_OpenDir(const wchar_t *dirname);
+BE_DIR_T BEL_Cross_OpenDir(const TCHAR *dirname);
 // Returns a pointer to the following directory entry's filename if any
 // is found, or a NULL pointer otherwise. This pointer should be assumed
 // to get invalidated after any other function call involving dir,
 // but it is valid to modify the string's contents beforehand.
-wchar_t *BEL_Cross_ReadDir(BE_DIR_T dir);
-
-#else
-
-BE_DIR_T BEL_Cross_OpenDir(const char *dirname);
-// Returns a pointer to the following directory entry's filename if any
-// is found, or a NULL pointer otherwise. This pointer should be assumed
-// to get invalidated after any other function call involving dir, but
-// but it is valid to modify the string's contents beforehand.
-char *BEL_Cross_ReadDir(BE_DIR_T dir);
-
-#endif
+TCHAR *BEL_Cross_ReadDir(BE_DIR_T dir);
 
 void BEL_Cross_RewindDir(BE_DIR_T dir);
 void BEL_Cross_CloseDir(BE_DIR_T dir);
