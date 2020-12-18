43e20504cf2789197d154b5d419f8ec79abba981
NY00123 <ny00@outlook.com>
2020-12-18 15:26:24 +0200

    Update a comment in currently present statetype_ptr_conversion.c files

--- src/id91_11/cat_all/catapoc/statetype_ptr_conversion.c
+++ src/id91_11/cat_all/catapoc/statetype_ptr_conversion.c
@@ -16,7 +16,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-// New file used for preparing and converting statetype objects offset
+// New file used for preparing and converting pointers to states
 
 #include "def.h"
 
