ReflectionHLE/ReflectionHLE

commit baddd70768401ed2bb3635f5efb143216eba3a42

Let's not initialize structs with variable-length arrays by using compound literals in be_gamedefs_*.h, be_st_sdl_launcher.c and the altcontroller.c files

src/backend/gamedefs/be_gamedefs_catapoc.h
@@ -66,10 +66,23 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_catapoc101[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_catapoc101_apocgame[] = {
+	{"AUDIODCT.APC", 1024, 0x26658498, 0x2439c},
+	{"AUDIOHHD.APC", 452, 0x76adb051, 0x1bd80},
+	{"EGADICT.APC", 1024, 0xb2ed57fd, 0x2479c},
+	{"EGAHEAD.APC", 2049, 0xd7548ed8, 0x1bf50},
+	{"MTEMP.TMP", 834, 0x90742162, 0x1ba30},
+	{0}
+};
+
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_catapoc101_hintcat[] = {
+	{"TEXTSCN.SCN", 4000, 0xf7773f42, 0x6dd0},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_catapoc101[] = {
 	{
 		NULL,
-
 		NULL,
 		"CATAPOC.EXE",
 		(void (*)(void))&REFKEEN_NS_ENCLOSE(catapoc, intro_exe_main),
@@ -79,15 +92,7 @@ static const BE_EXEFileDetails_T g_be_exefiles_catapoc101[] = {
 		false
 	},
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"AUDIODCT.APC", 1024, 0x26658498, 0x2439c},
-			{"AUDIOHHD.APC", 452, 0x76adb051, 0x1bd80},
-			{"EGADICT.APC", 1024, 0xb2ed57fd, 0x2479c},
-			{"EGAHEAD.APC", 2049, 0xd7548ed8, 0x1bf50},
-			{"MTEMP.TMP", 834, 0x90742162, 0x1ba30},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_catapoc101_apocgame,
 		NULL,
 		"APOCGAME.EXE",
 		&REFKEEN_NS_ENCLOSE(catapoc, apocgame_exe_main),
@@ -97,12 +102,7 @@ static const BE_EXEFileDetails_T g_be_exefiles_catapoc101[] = {
 		false
 	},
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"TEXTSCN.SCN", 4000, 0xf7773f42, 0x6ef0},
-			{0}
-		},
-
-
+		g_be_embeddedgamefiles_catapoc101_hintcat,
 		"Catacomb Apocalypse 3-D Hint Book v1.13",
 		"HINTCAT.EXE",
 		&REFKEEN_NS_ENCLOSE(catapoc, slidecat_exe_main),

src/backend/gamedefs/be_gamedefs_cat3d.h
@@ -28,18 +28,19 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_cat3d100[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_cat3d100[] = {
+	{"AUDIODCT.C3D", 1024, 0xd3dbe849, 0x22c64},
+	{"AUDIOHHD.C3D", 368, 0xb83933bc, 0x1aa60},
+	{"EGADICT.C3D", 1024, 0xab94fb6c, 0x23064},
+	{"EGAHEAD.C3D", 1437, 0x33772bb0, 0x1abd0},
+	{"INTROSCN.SCN", 4008, 0xec236c5c, 0x192c0},
+	{"MTEMP.TMP", 618, 0x6b7cc556, 0x1b170},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_cat3d100[] = {
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"AUDIODCT.C3D", 1024, 0xd3dbe849, 0x22c64},
-			{"AUDIOHHD.C3D", 368, 0xb83933bc, 0x1aa60},
-			{"EGADICT.C3D", 1024, 0xab94fb6c, 0x23064},
-			{"EGAHEAD.C3D", 1437, 0x33772bb0, 0x1abd0},
-			{"INTROSCN.SCN", 4008, 0xec236c5c, 0x192c0},
-			{"MTEMP.TMP", 618, 0x6b7cc556, 0x1b170},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_cat3d100,
 		NULL,
 		"CAT3D.EXE",
 		&REFKEEN_NS_ENCLOSE(cat3d, cat3d_exe_main),
@@ -70,20 +71,21 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_cat3d122[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_cat3d122[] = {
+	{"AUDIODCT.C3D", 1024, 0xd3dbe849, 0x22bd8},
+	{"AUDIOHHD.C3D", 368, 0xb83933bc, 0x1a710},
+	{"EGADICT.C3D", 1024, 0xb26a70a6, 0x22fd8},
+	{"EGAHEAD.C3D", 1437, 0x3fde00c4, 0x1a880},
+	// INTROSCN.SCN isn't displayed in vanilla v1.22, but it's still
+	// allocated and in use, so it's safer to require this chunk
+	{"INTROSCN.SCN", 4008, 0xcf9696af, 0x18f70},
+	{"MTEMP.TMP", 618, 0x6b7cc556, 0x1ae20},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_cat3d122[] = {
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"AUDIODCT.C3D", 1024, 0xd3dbe849, 0x22bd8},
-			{"AUDIOHHD.C3D", 368, 0xb83933bc, 0x1a710},
-			{"EGADICT.C3D", 1024, 0xb26a70a6, 0x22fd8},
-			{"EGAHEAD.C3D", 1437, 0x3fde00c4, 0x1a880},
-			// INTROSCN.SCN isn't displayed in vanilla v1.22, but it's still
-			// allocated and in use, so it's safer to require this chunk
-			{"INTROSCN.SCN", 4008, 0xcf9696af, 0x18f70},
-			{"MTEMP.TMP", 618, 0x6b7cc556, 0x1ae20},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_cat3d122,
 		NULL,
 		"CAT3D.EXE",
 		&REFKEEN_NS_ENCLOSE(cat3d, cat3d_exe_main),

src/backend/gamedefs/be_gamedefs_catabyss.h
@@ -77,11 +77,24 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_catabyss113[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_catabyss113_catabyss[] = {
+	{"AUDIODCT.ABS", 1024, 0xe9088011, 0x2554c},
+	{"AUDIOHHD.ABS", 416, 0xfbfff495, 0x1a210},
+	{"EGADICT.ABS", 1024, 0xbb760f1d, 0x2594c},
+	{"EGAHEAD.ABS", 1881, 0xe31e1c3b, 0x1a3b0},
+	{"MTEMP.TMP", 834, 0x5d9ccfb3, 0x1ab10},
+	{0}
+};
+
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_catabyss113_democat[] = {
+	{"TEXTSCN.SCN", 4000, 0xf7773f42, 0xbf70},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_catabyss113[] = {
 	// Intro EXE is the first one we begin from
 	{
 		NULL,
-
 		NULL,
 		"INTRO.EXE",
 		(void (*)(void))&REFKEEN_NS_ENCLOSE(catabyss, intro_exe_main),
@@ -91,15 +104,7 @@ static const BE_EXEFileDetails_T g_be_exefiles_catabyss113[] = {
 		false
 	},
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"AUDIODCT.ABS", 1024, 0xe9088011, 0x2554c},
-			{"AUDIOHHD.ABS", 416, 0xfbfff495, 0x1a210},
-			{"EGADICT.ABS", 1024, 0xbb760f1d, 0x2594c},
-			{"EGAHEAD.ABS", 1881, 0xe31e1c3b, 0x1a3b0},
-			{"MTEMP.TMP", 834, 0x5d9ccfb3, 0x1ab10},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_catabyss113_catabyss,
 		NULL,
 		"CATABYSS.EXE",
 		&REFKEEN_NS_ENCLOSE(catabyss, abysgame_exe_main),
@@ -109,11 +114,7 @@ static const BE_EXEFileDetails_T g_be_exefiles_catabyss113[] = {
 		false
 	},
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"TEXTSCN.SCN", 4000, 0xf7773f42, 0xbf70},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_catabyss113_democat,
 		"Electronic Catalog v1.00",
 		"DEMOCAT.EXE",
 		&REFKEEN_NS_ENCLOSE(catabyss, slidecat_exe_main),
@@ -124,7 +125,6 @@ static const BE_EXEFileDetails_T g_be_exefiles_catabyss113[] = {
 	},
 	{
 		NULL,
-
 		NULL,
 		"LOADSCN.EXE",
 		&REFKEEN_NS_ENCLOSE(catabyss, loadscn_exe_main),
@@ -191,11 +191,24 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_catabyss124[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_catabyss124_abysgame[] = {
+	{"AUDIODCT.ABS", 1024, 0xe9088011, 0x2543a},
+	{"AUDIOHHD.ABS", 416, 0xfbfff495, 0x1a140},
+	{"EGADICT.ABS", 1024, 0x63eb06d3, 0x2583a},
+	{"EGAHEAD.ABS", 1881, 0x94967205, 0x1a2e0},
+	{"MTEMP.TMP", 834, 0x5d9ccfb3, 0x1aa40},
+	{0}
+};
+
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_catabyss124_hintcat[] = {
+	{"TEXTSCN.SCN", 4000, 0xf7773f42, 0x6f60},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_catabyss124[] = {
 	// Again, intro EXE is the first one we begin from
 	{
 		NULL,
-
 		NULL,
 		"CATABYSS.EXE",
 		(void (*)(void))&REFKEEN_NS_ENCLOSE(catabyss, intro_exe_main),
@@ -205,15 +218,7 @@ static const BE_EXEFileDetails_T g_be_exefiles_catabyss124[] = {
 		false
 	},
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"AUDIODCT.ABS", 1024, 0xe9088011, 0x2543a},
-			{"AUDIOHHD.ABS", 416, 0xfbfff495, 0x1a140},
-			{"EGADICT.ABS", 1024, 0x63eb06d3, 0x2583a},
-			{"EGAHEAD.ABS", 1881, 0x94967205, 0x1a2e0},
-			{"MTEMP.TMP", 834, 0x5d9ccfb3, 0x1aa40},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_catabyss124_abysgame,
 		NULL,
 		"ABYSGAME.EXE",
 		&REFKEEN_NS_ENCLOSE(catabyss, abysgame_exe_main),
@@ -223,11 +228,7 @@ static const BE_EXEFileDetails_T g_be_exefiles_catabyss124[] = {
 		false
 	},
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"TEXTSCN.SCN", 4000, 0xf7773f42, 0x6f60},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_catabyss124_hintcat,
 		"Catacomb Abyss 3-D Hint Book v1.01",
 		"HINTCAT.EXE",
 		&REFKEEN_NS_ENCLOSE(catabyss, slidecat_exe_main),

src/backend/gamedefs/be_gamedefs_catarm.h
@@ -67,10 +67,23 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_catarm102[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_catarm102_armgame[] = {
+	{"AUDIODCT.ARM", 1024, 0x8f1d4dd2, 0x240b0},
+	{"AUDIOHHD.ARM", 428, 0x5f863ad2, 0x1bb20},
+	{"EGADICT.ARM", 1024, 0xab662db8, 0x244b0},
+	{"EGAHEAD.ARM", 1977, 0x711cbf10, 0x1bcd0},
+	{"MTEMP.TMP", 834, 0x546f00d1, 0x1b7d0},
+	{0}
+};
+
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_catarm102_hintcat[] = {
+	{"TEXTSCN.SCN", 4000, 0xf7773f42, 0x6dd0},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_catarm102[] = {
 	{
 		NULL,
-
 		NULL,
 		"CATARM.EXE",
 		(void (*)(void))&REFKEEN_NS_ENCLOSE(catarm, intro_exe_main),
@@ -80,15 +93,7 @@ static const BE_EXEFileDetails_T g_be_exefiles_catarm102[] = {
 		false
 	},
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"AUDIODCT.ARM", 1024, 0x8f1d4dd2, 0x240b0},
-			{"AUDIOHHD.ARM", 428, 0x5f863ad2, 0x1bb20},
-			{"EGADICT.ARM", 1024, 0xab662db8, 0x244b0},
-			{"EGAHEAD.ARM", 1977, 0x711cbf10, 0x1bcd0},
-			{"MTEMP.TMP", 834, 0x546f00d1, 0x1b7d0},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_catarm102_armgame,
 		NULL,
 		"ARMGAME.EXE",
 		&REFKEEN_NS_ENCLOSE(catarm, armgame_exe_main),
@@ -98,11 +103,7 @@ static const BE_EXEFileDetails_T g_be_exefiles_catarm102[] = {
 		false
 	},
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"TEXTSCN.SCN", 4000, 0xf7773f42, 0x6dd0},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_catarm102_hintcat,
 		"Catacomb Armageddon 3-D Hint Book v1.12",
 		"HINTCAT.EXE",
 		&REFKEEN_NS_ENCLOSE(catarm, slidecat_exe_main),

src/backend/gamedefs/be_gamedefs_kdreams.h
@@ -35,22 +35,23 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_kdreamse100[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_kdreamse100[] = {
+	{"AUDIODCT.KDR", 1024, 0x8b6116d7, 0x28984},
+	{"AUDIOHHD.KDR", 340, 0x499e0cbf, 0x1f370},
+	{"CONTEXT.KDR", 4759, 0x5bae2337, 0x1f4d0},
+	{"EGADICT.KDR", 1024, 0xa69af202, 0x28188},
+	{"EGAHEAD.KDR", 12068, 0xb9d789ee, 0x19610},
+	{"GAMETEXT.KDR", 4686, 0x046c5328, 0x20770},
+	{"MAPDICT.KDR", 1020, 0x8aa7334b, 0x28588},
+	{"MAPHEAD.KDR", 11824, 0x4b9c9ebe, 0x1c540},
+	{"PIRACY.BIN", 4001, 0x94458def, 0x14970}, // A bit different from PIRACY.SCN
+	{"STORY.KDR", 2487, 0xed0ea5fe, 0x219c0},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_kdreamse100[] = {
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"AUDIODCT.KDR", 1024, 0x8b6116d7, 0x28984},
-			{"AUDIOHHD.KDR", 340, 0x499e0cbf, 0x1f370},
-			{"CONTEXT.KDR", 4759, 0x5bae2337, 0x1f4d0},
-			{"EGADICT.KDR", 1024, 0xa69af202, 0x28188},
-			{"EGAHEAD.KDR", 12068, 0xb9d789ee, 0x19610},
-			{"GAMETEXT.KDR", 4686, 0x046c5328, 0x20770},
-			{"MAPDICT.KDR", 1020, 0x8aa7334b, 0x28588},
-			{"MAPHEAD.KDR", 11824, 0x4b9c9ebe, 0x1c540},
-			{"PIRACY.BIN", 4001, 0x94458def, 0x14970}, // A bit different from PIRACY.SCN
-			{"STORY.KDR", 2487, 0xed0ea5fe, 0x219c0},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_kdreamse100,
 		NULL,
 		"KDREAMS.EXE",
 		&kdreams_exe_main,
@@ -81,22 +82,23 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_kdreamsc100[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_kdreamsc100[] = {
+	{"AUDIODCT.KDR", 1024, 0x8b6116d7, 0x281c2},
+	{"AUDIOHHD.KDR", 340, 0x499e0cbf, 0x1e6b0},
+	{"CGADICT.KDR", 1024, 0xaba89759, 0x279c6},
+	{"CGAHEAD.KDR", 12068, 0x36d48226, 0x18950},
+	{"CONTEXT.KDR", 4759, 0x5bae2337, 0x1e810},
+	{"GAMETEXT.KDR", 4686, 0x046c5328, 0x1fab0},
+	{"MAPDICT.KDR", 1020, 0x8aa7334b, 0x27dc6},
+	{"MAPHEAD.KDR", 11824, 0x4b9c9ebe, 0x1b880},
+	{"PIRACY.BIN", 4001, 0x94458def, 0x13cb0}, // A bit different from PIRACY.SCN
+	{"STORY.KDR", 2487, 0xed0ea5fe, 0x20d00},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_kdreamsc100[] = {
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"AUDIODCT.KDR", 1024, 0x8b6116d7, 0x281c2},
-			{"AUDIOHHD.KDR", 340, 0x499e0cbf, 0x1e6b0},
-			{"CGADICT.KDR", 1024, 0xaba89759, 0x279c6},
-			{"CGAHEAD.KDR", 12068, 0x36d48226, 0x18950},
-			{"CONTEXT.KDR", 4759, 0x5bae2337, 0x1e810},
-			{"GAMETEXT.KDR", 4686, 0x046c5328, 0x1fab0},
-			{"MAPDICT.KDR", 1020, 0x8aa7334b, 0x27dc6},
-			{"MAPHEAD.KDR", 11824, 0x4b9c9ebe, 0x1b880},
-			{"PIRACY.BIN", 4001, 0x94458def, 0x13cb0}, // A bit different from PIRACY.SCN
-			{"STORY.KDR", 2487, 0xed0ea5fe, 0x20d00},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_kdreamsc100,
 		NULL,
 		"KDREAMS.EXE",
 		&kdreams_exe_main,
@@ -129,21 +131,22 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_kdreamse113[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_kdreamse113[] = {
+	{"AUDIODCT.KDR", 1024, 0x8b6116d7, 0x2a042},
+	{"AUDIOHHD.KDR", 340, 0x499e0cbf, 0x22880},
+	{"CONTEXT.KDR", 1283, 0x5a33439d, 0x229e0},
+	{"EGADICT.KDR", 1024, 0xa69af202, 0x29846},
+	{"EGAHEAD.KDR", 12068, 0xb9d789ee, 0x1cb20},
+	{"GAMETEXT.KDR", 413, 0xb0df2792, 0x22ef0},
+	{"MAPDICT.KDR", 1020, 0x9faa7213, 0x29c46},
+	{"MAPHEAD.KDR", 11824, 0xb2f36c60, 0x1fa50},
+	{"STORY.KDR", 2526, 0xcafc1d15, 0x23090},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_kdreamse113[] = {
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"AUDIODCT.KDR", 1024, 0x8b6116d7, 0x2a042},
-			{"AUDIOHHD.KDR", 340, 0x499e0cbf, 0x22880},
-			{"CONTEXT.KDR", 1283, 0x5a33439d, 0x229e0},
-			{"EGADICT.KDR", 1024, 0xa69af202, 0x29846},
-			{"EGAHEAD.KDR", 12068, 0xb9d789ee, 0x1cb20},
-			{"GAMETEXT.KDR", 413, 0xb0df2792, 0x22ef0},
-			{"MAPDICT.KDR", 1020, 0x9faa7213, 0x29c46},
-			{"MAPHEAD.KDR", 11824, 0xb2f36c60, 0x1fa50},
-			{"STORY.KDR", 2526, 0xcafc1d15, 0x23090},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_kdreamse113,
 		NULL,
 		"KDREAMS.EXE",
 		&kdreams_exe_main,
@@ -154,7 +157,6 @@ static const BE_EXEFileDetails_T g_be_exefiles_kdreamse113[] = {
 	},
 	{
 		NULL,
-
 		NULL,
 		"LOADSCN.EXE",
 		(void (*)(void))&loadscn2_main,
@@ -185,21 +187,22 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_kdreamsc105[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_kdreamsc105[] = {
+	{"AUDIODCT.KDR", 1024, 0x8b6116d7, 0x28490},
+	{"AUDIOHHD.KDR", 340, 0x499e0cbf, 0x1eb50},
+	{"CGADICT.KDR", 1024, 0xaba89759, 0x27c94},
+	{"CGAHEAD.KDR", 12068, 0x36d48226, 0x18df0},
+	{"CONTEXT.KDR", 4759, 0x5bae2337, 0x1ecb0},
+	{"GAMETEXT.KDR", 4686, 0x046c5328, 0x1ff50},
+	{"MAPDICT.KDR", 1020, 0xfa8362f3, 0x28094},
+	{"MAPHEAD.KDR", 11824, 0x66c122b4, 0x1bd20},
+	{"STORY.KDR", 2487, 0xed0ea5fe, 0x211a0},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_kdreamsc105[] = {
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"AUDIODCT.KDR", 1024, 0x8b6116d7, 0x28490},
-			{"AUDIOHHD.KDR", 340, 0x499e0cbf, 0x1eb50},
-			{"CGADICT.KDR", 1024, 0xaba89759, 0x27c94},
-			{"CGAHEAD.KDR", 12068, 0x36d48226, 0x18df0},
-			{"CONTEXT.KDR", 4759, 0x5bae2337, 0x1ecb0},
-			{"GAMETEXT.KDR", 4686, 0x046c5328, 0x1ff50},
-			{"MAPDICT.KDR", 1020, 0xfa8362f3, 0x28094},
-			{"MAPHEAD.KDR", 11824, 0x66c122b4, 0x1bd20},
-			{"STORY.KDR", 2487, 0xed0ea5fe, 0x211a0},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_kdreamsc105,
 		NULL,
 		"KDREAMS.EXE",
 		&kdreams_exe_main,
@@ -234,21 +237,22 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_kdreamse193[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_kdreamse193[] = {
+	{"AUDIODCT.KDR", 1024, 0x8b6116d7, 0x29fba},
+	{"AUDIOHHD.KDR", 340, 0x499e0cbf, 0x21990},
+	{"CONTEXT.KDR", 1283, 0x5a33439d, 0x21af0},
+	{"EGADICT.KDR", 1024, 0xa69af202, 0x297be},
+	{"EGAHEAD.KDR", 12068, 0xb9d789ee, 0x1bc30},
+	{"GAMETEXT.KDR", 4256, 0xbfe72f94, 0x22000},
+	{"MAPDICT.KDR", 1020, 0x6bb0de32, 0x29bbe},
+	{"MAPHEAD.KDR", 11824, 0x2b821e29, 0x1eb60},
+	{"STORY.KDR", 2526, 0xcafc1d15, 0x230a0},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_kdreamse193[] = {
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"AUDIODCT.KDR", 1024, 0x8b6116d7, 0x29fba},
-			{"AUDIOHHD.KDR", 340, 0x499e0cbf, 0x21990},
-			{"CONTEXT.KDR", 1283, 0x5a33439d, 0x21af0},
-			{"EGADICT.KDR", 1024, 0xa69af202, 0x297be},
-			{"EGAHEAD.KDR", 12068, 0xb9d789ee, 0x1bc30},
-			{"GAMETEXT.KDR", 4256, 0xbfe72f94, 0x22000},
-			{"MAPDICT.KDR", 1020, 0x6bb0de32, 0x29bbe},
-			{"MAPHEAD.KDR", 11824, 0x2b821e29, 0x1eb60},
-			{"STORY.KDR", 2526, 0xcafc1d15, 0x230a0},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_kdreamse193,
 		NULL,
 		"KDREAMS.EXE",
 		&kdreams_exe_main,
@@ -280,21 +284,22 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_kdreamse120[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_kdreamse120[] = {
+	{"AUDIODCT.KDR", 1024, 0x8b6116d7, 0x2a66c},
+	{"AUDIOHHD.KDR", 340, 0x499e0cbf, 0x21d70},
+	{"CONTEXT.KDR", 1283, 0x5a33439d, 0x21ed0},
+	{"EGADICT.KDR", 1024, 0xa69af202, 0x29e70},
+	{"EGAHEAD.KDR", 12068, 0xb9d789ee, 0x1c010},
+	{"GAMETEXT.KDR", 4256, 0xbfe72f94, 0x223e0},
+	{"MAPDICT.KDR", 1020, 0x6bb0de32, 0x2a270},
+	{"MAPHEAD.KDR", 11824, 0x2b821e29, 0x1ef40},
+	{"STORY.KDR", 2526, 0xcafc1d15, 0x23480},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_kdreamse120[] = {
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"AUDIODCT.KDR", 1024, 0x8b6116d7, 0x2a66c},
-			{"AUDIOHHD.KDR", 340, 0x499e0cbf, 0x21d70},
-			{"CONTEXT.KDR", 1283, 0x5a33439d, 0x21ed0},
-			{"EGADICT.KDR", 1024, 0xa69af202, 0x29e70},
-			{"EGAHEAD.KDR", 12068, 0xb9d789ee, 0x1c010},
-			{"GAMETEXT.KDR", 4256, 0xbfe72f94, 0x223e0},
-			{"MAPDICT.KDR", 1020, 0x6bb0de32, 0x2a270},
-			{"MAPHEAD.KDR", 11824, 0x2b821e29, 0x1ef40},
-			{"STORY.KDR", 2526, 0xcafc1d15, 0x23480},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_kdreamse120,
 		NULL,
 		"KDREAMS.EXE",
 		&kdreams_exe_main,
@@ -340,7 +345,6 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_kdreams2015[] = {
 static const BE_EXEFileDetails_T g_be_exefiles_kdreams2015[] = {
 	{
 		NULL,
-
 		NULL,
 		NULL, // No EXE file
 		&kdreams_exe_main,

src/backend/gamedefs/be_gamedefs_n3dwt10.h
@@ -29,13 +29,14 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_n3dwt10[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_n3dwt10[] = {
+	{"GAMEPAL.BIN", 768, 0xc76ff953, 0x280e0},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_n3dwt10[] = {
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"GAMEPAL.BIN", 768, 0xc76ff953, 0x280e0},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_n3dwt10,
 		NULL,
 		"NOAH3DOS.EXE", // FIXME: See above
 		&REFKEEN_NS_ENCLOSE(n3dwt10, wolf3d_exe_main),

src/backend/gamedefs/be_gamedefs_sdmfg10.h
@@ -29,14 +29,15 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_sdmfg10[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_sdmfg10[] = {
+	{"GAMEPAL.BIN", 768, 0xbd030b1d, 0x2fef0},
+	{"SIGNON.BIN", 64000, 0x07cf2545, 0x204f0},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_sdmfg10[] = {
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"GAMEPAL.BIN", 768, 0xbd030b1d, 0x2fef0},
-			{"SIGNON.BIN", 64000, 0x07cf2545, 0x204f0},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_sdmfg10,
 		NULL,
 		"SOD.EXE",
 		&REFKEEN_NS_ENCLOSE(sdmfg10, wolf3d_exe_main),

src/backend/gamedefs/be_gamedefs_sodac14.h
@@ -29,14 +29,15 @@ static const BE_GameFileDetails_T g_be_reqgameverfiles_sodac14[] = {
 	{0}
 };
 
+static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_sodac14[] = {
+	{"GAMEPAL.BIN", 768, 0xbd030b1d, 0x3d35c},
+	{"SIGNON.BIN", 64000, 0x1644999b, 0x24690},
+	{0}
+};
+
 static const BE_EXEFileDetails_T g_be_exefiles_sodac14[] = {
 	{
-		(const BE_EmbeddedGameFileDetails_T []) {
-			{"GAMEPAL.BIN", 768, 0xbd030b1d, 0x3d35c},
-			{"SIGNON.BIN", 64000, 0x1644999b, 0x24690},
-			{0}
-		},
-
+		g_be_embeddedgamefiles_sodac14,
 		NULL,
 		"SPEAR.EXE",
 		&REFKEEN_NS_ENCLOSE(sodac14, wolf3d_exe_main),

commit 66300043bf264c26b05800dcfe04fc5ace7717d7

catapoc: Fix a typo introduced in baddd70768401ed2bb3635f5efb143216eba3a42

src/backend/gamedefs/be_gamedefs_catapoc.h
@@ -76,7 +76,7 @@ static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_catapoc101_apoc
 };
 
 static const BE_EmbeddedGameFileDetails_T g_be_embeddedgamefiles_catapoc101_hintcat[] = {
-	{"TEXTSCN.SCN", 4000, 0xf7773f42, 0x6dd0},
+	{"TEXTSCN.SCN", 4000, 0xf7773f42, 0x6ef0},
 	{0}
 };
 