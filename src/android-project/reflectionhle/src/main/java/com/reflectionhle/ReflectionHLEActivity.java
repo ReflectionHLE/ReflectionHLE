dadac9e1ee6668c0b608a6d21d2bfa4e24eb5999
NY00123 <ny00@outlook.com>
2024-07-13 22:16:32 +0300

    Improve support for Android 11 (API level 30) and later versions. Due to the introduction of scoped storage, the permission MANAGE_EXTERNAL_STORAGE is now requested on Android 11+ instead of READ_EXTERNAL_STORAGE. Technically speaking, a side-effect of this is allowing greater write access, even if ReflectionHLE has no need for it, but that's been a simple solution for now, at least for manually downloaded APK files.

--- src/android-project/reflectionhle/src/main/java/com/reflectionhle/ReflectionHLEActivity.java
+++ src/android-project/reflectionhle/src/main/java/com/reflectionhle/ReflectionHLEActivity.java
@@ -4,14 +4,44 @@ import org.libsdl.app.SDLActivity;
 import android.view.View;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import android.Manifest;
+import android.net.Uri;
+import android.provider.Settings;
 
 public class ReflectionHLEActivity extends SDLActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
 	private final int[] requestNotif = new int[1];
+	private final Object alertNotif = new Object();
+
+	class PermissionRequestExplanation implements Runnable {
+		@Override
+		public void run() {
+			AlertDialog.Builder builder = new AlertDialog.Builder(mSingleton);
+			builder.setMessage("Thanks for your interest in ReflectionHLE. Allowing access to shared storage should let you select a supported game installation from more locations.");
+			builder.setTitle("Access to shared storage requested");
+			builder.setNeutralButton("Continue", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					synchronized (alertNotif) {
+						alertNotif.notify();
+					}
+				}
+			});
+			builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+				public void onDismiss(final DialogInterface dialog) {
+					synchronized (alertNotif) {
+						alertNotif.notify();
+					}
+				}
+			});
+
+			AlertDialog dialog = builder.create();
+			dialog.show();
+		}
+	}
 
 	private void waitForUIThread(Object notif) {
 		synchronized (notif) {
@@ -24,38 +54,30 @@ public class ReflectionHLEActivity extends SDLActivity implements ActivityCompat
 	}
 
 	public int requestReadExternalStoragePermission() {
-		if (android.os.Build.VERSION.SDK_INT >= 23) {
+		if (android.os.Build.VERSION.SDK_INT >= 30) {
+			if (!android.os.Environment.isExternalStorageManager()) {
+				if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.MANAGE_EXTERNAL_STORAGE)) {
+					runOnUiThread(new PermissionRequestExplanation());
+					// Wait for onClick from dialog button
+					waitForUIThread(alertNotif);
+				}
+
+				try {
+					Intent intent = new Intent();
+					intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+					Uri uri = Uri.fromParts("package", this.getPackageName(), null);
+					intent.setData(uri);
+					startActivity(intent);
+				} catch (Exception e) {
+					Intent intent = new Intent();
+					intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+					startActivity(intent);
+				}
+			}
+		} else if (android.os.Build.VERSION.SDK_INT >= 23) {
 			if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
 				if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-
-					final Object alertNotif = new Object();
-
-					runOnUiThread(new Runnable() {
-						@Override
-						public void run() {
-							AlertDialog.Builder builder = new AlertDialog.Builder(mSingleton);
-							builder.setMessage("Thanks for your interest in ReflectionHLE. Allowing access to shared storage should let you select a supported game installation from more locations.");
-							builder.setTitle("Access to shared storage requested");
-							builder.setNeutralButton("Continue", new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog, int id) {
-									synchronized (alertNotif) {
-										alertNotif.notify();
-									}
-								}
-							});
-							builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-								public void onDismiss(final DialogInterface dialog) {
-									synchronized (alertNotif) {
-										alertNotif.notify();
-									}
-								}
-							});
-
-							AlertDialog dialog = builder.create();
-							dialog.show();
-						}
-					});
-
+					runOnUiThread(new PermissionRequestExplanation());
 					// Wait for onClick from dialog button
 					waitForUIThread(alertNotif);
 				}
