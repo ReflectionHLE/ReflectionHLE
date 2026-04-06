99ab07e963796b4990e8ec205657c96c7f2626ca
NY00123 <ny00@outlook.com>
2026-04-06 18:12:38 +0300

    Important be_mutex_sdl.h fix: Asserts translate to no-ops in release builds! We've probably been lucky to not have a crash reported for such a build, or none has been correlated. Removing the asserts is simple enough.
    On a side-note, that's also the first commit preparing for migration to SDL3.

--- src/backend/threading/be_mutex_sdl.h
+++ src/backend/threading/be_mutex_sdl.h
@@ -8,7 +8,7 @@ typedef SDL_mutex BE_ST_Mutex_T;
 
 static inline BE_ST_Mutex_T *BE_ST_CreateMutex(void) { return SDL_CreateMutex(); }
 static inline void BE_ST_FreeMutex(BE_ST_Mutex_T *mutex) { SDL_DestroyMutex(mutex); }
-static inline void BE_ST_LockMutexRecursively(BE_ST_Mutex_T *mutex) { assert(!SDL_LockMutex(mutex)); }
-static inline void BE_ST_UnlockMutexRecursively(BE_ST_Mutex_T *mutex) { assert(!SDL_UnlockMutex(mutex)); }
+static inline void BE_ST_LockMutexRecursively(BE_ST_Mutex_T *mutex) { SDL_LockMutex(mutex); }
+static inline void BE_ST_UnlockMutexRecursively(BE_ST_Mutex_T *mutex) { SDL_UnlockMutex(mutex); }
 
 #endif
